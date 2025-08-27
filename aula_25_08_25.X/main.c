df3117fe4044d5aec7365ff513404ef4bdbab1c0
Daleffe <augustodaleffe@gmail.com>
2025-08-27 15:25:01 -0300

    aula 25 final RoundRobin

--- aula_25_08_25.X/main.c
+++ aula_25_08_25.X/main.c
@@ -1,119 +1,161 @@
 #include <xc.h>
+#include <stdint.h>
+
+#define QUANTUM 10  
 
 typedef void (*task_ptr)(void);
-
 
 typedef enum {READY = 0, RUNNING, WAITING} state_t;
 
 typedef struct tcb {
     unsigned int id;
-    task_ptr   func;
-    state_t    task_state;
+    task_ptr func;
+    state_t task_state;
 } tcb_t;
 
+//Chamadas de sistema
+unsigned int rr_scheduler();
+void create_task(unsigned int id, task_ptr task);
+void change_state(state_t new_state);
 
-//Configs
-void config_times();
-void config_ports();
+// Configuração
+void config_timer(void);
+void config_ports(void);
 
 
-//Tasks
+
+// Tarefas
 void task_1();
 void task_2();
 void task_3();
 
+// ISR timer
+void __interrupt() ISR_TIMER();
 
-// IRS TIMER
-void __interrupt() ISB_TIMES();
-
-// Global enviroments
+// Variáveis globais
 tcb_t f_aptos[3];
-unsigned int vez = 0, qtd_tasks = 0;
+unsigned int vez = 0, qtd_tasks = 0, quantum = 0;
 
-    asm("GLOBAL _task_1, _task_2, _task_3");
+asm("GLOBAL _task_1, _task_2, _task_3");
 
-int main(){
-    
-    config_times();
+int main()
+{
+    config_timer();
     config_ports();
     
-    //Criar as tarefas
+    // Criar as tarefas
+    create_task(1, task_1);
+    create_task(2, task_2);
+    create_task(3, task_3);
     
-    f_aptos[qtd_tasks].id       = 1;
-    f_aptos[qtd_tasks].func     = task_1 ;
-    f_aptos[qtd_tasks++].task_state      = READY;
+    // Define o tempo que cada tarefa vai executar
+    quantum = QUANTUM; 
     
-    f_aptos[qtd_tasks].id               = 2;
-    f_aptos[qtd_tasks].func             = task_2 ;
-    f_aptos[qtd_tasks++].task_state     = READY;
-    
-    f_aptos[qtd_tasks].id               = 3;
-    f_aptos[qtd_tasks].func             = task_3 ;
-    f_aptos[qtd_tasks++].task_state     = READY;
-    
-    // Enable global interrupts
+    // Habilitar as interrupções globais
     ei();
     
-    while(1);
+    while (1);
     
     return 0;
+}
+
+// Escalonador de processos
+unsigned int rr_scheduler(){
+    do{
+        vez = (vez+1) %3;
+        if(f_aptos[vez].task_state == READY) return vez;
+        
+    } while (1);
+}
+
+void create_task(unsigned int id, task_ptr task){
+    f_aptos[qtd_tasks].id   = id;
+    f_aptos[qtd_tasks].func   = task;
+    f_aptos[qtd_tasks++].task_state   = READY;
     
 }
 
-void config_times()
+void change_state(state_t new_state){
+    
+    //Desabilitar as interrupções
+    di();
+    
+    f_aptos[vez].task_state = new_state;
+    
+    // Troca de contexto 
+    STKPTR  = 0;
+        
+    asm("push");
+
+    TOS     = (uint24_t)f_aptos[rr_scheduler()].func;
+    
+    // Habilita as interrupções
+    ei();
+    
+    
+}
+
+// Configuração
+void config_timer()
 {
     T0CONbits.T0CS      = 0;
-    T0CONbits.PSA       = 1;
-    T0CONbits.T0PS      = 0b111;
+    T0CONbits.PSA       = 0;
+    T0CONbits.T0PS      = 0b110;    // 1:128
     INTCONbits.TMR0IE   = 1;
     INTCONbits.TMR0IF   = 0;
     INTCONbits.PEIE     = 1;
     TMR0L               = 10;
-    T0CONbits.TMR0ON    = 1;
-    
-    
+    T0CONbits.TMR0ON    = 1;    
 }
 
-
-void config_ports(){
-    
-    TRISDbits.RD0  = 0;
+void config_ports()
+{
+    TRISDbits.RD0   = 0;
     TRISDbits.RD1   = 0;
     TRISDbits.RD2   = 0;
-    
-    
 }
 
-void task_1(){
-    while(1){
+// Tarefas
+void task_1()
+{
+    while (1) {
         LATDbits.LD0    = ~PORTDbits.RD0;
     }
-    
 }
 
-void task_2(){
-   LATDbits.LD1    = ~PORTDbits.RD1; 
+void task_2()
+{
+    while (1) {
+        LATDbits.LD1    = ~PORTDbits.RD1;    
+    }
 }
 
-void task_3(){    
-   LATDbits.LD2    = ~PORTDbits.RD2; 
+void task_3()
+{
+    while (1) {
+        LATDbits.LD2    = ~PORTDbits.RD2;    
+        change_state(WAITING);
+    }
 }
 
+// ISR timer
+void __interrupt() ISR_TIMER()
+{
+    if (INTCONbits.TMR0IF == 1) {
+        INTCONbits.TMR0IF   = 0;
+        TMR0L               = 10;
+        quantum--;
+        
+        if(quantum == 0){
+            quantum = QUANTUM;
+            
+            STKPTR  = 0;
+        
+            asm("push");
 
-void __interrupt() ISB_TIMER(){
-    
-    if(INTCONbits.TMR0IF ==1){
-        INTCONbits.TMR0IF = 0;
-        TMR0L             = 10;
-        
-        
-        STKPTR            = 0;
-        asm("PUSH");
-        TOS               = (uint24_t)f_aptos[vez].func;
-      
-        vez = (vez+1) % qtd_tasks;
-        
-    }    
+            TOS     = (uint24_t)f_aptos[rr_scheduler()].func;
+        }     
 
-    
+    }
+
 }
