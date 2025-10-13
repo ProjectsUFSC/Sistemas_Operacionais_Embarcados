8a36a112f9081c5bb147ec445d6bb47c7f767e88
Daleffe <augustodaleffe@gmail.com>
2025-10-13 14:24:28 -0300

    update aula freeRTOS

--- FreeRTOS/user_app.c
+++ FreeRTOS/user_app.c
@@ -3,16 +3,26 @@
 #include "user_app.h"
 #include "FreeRTOS.h"
 #include <xc.h>
+#include "semphr.h"
 
+
+
+SemaphoreHandle_t s[5];
 
 void config_user_app()
 {
     TRISDbits.TRISD0 = 0;
+    TRISDbits.TRISD1 = 0;
+    for(int i = 0; i<sizeof(s); i++){
+        s[i] = xSemaphoreCreateBinary();
+    }
+    
 }
 
 void task_1()
 {
     while (1) {
         LATDbits.LATD0 = ~PORTDbits.RD0;
+        
     }
 }
