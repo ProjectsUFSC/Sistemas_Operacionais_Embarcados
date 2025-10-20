8a684b83a70efd6edb956b861e87547fbc8c79ef
Daleffe <augustodaleffe@gmail.com>
2025-10-20 16:00:00 -0300

    autalizacao aula 20 outubro

--- FreeRTOS/main.c
+++ FreeRTOS/main.c
@@ -8,10 +8,36 @@ int main()
     
     xTaskCreate(task_1, "T1", configMINIMAL_STACK_SIZE, NULL, 3, NULL);
     xTaskCreate(task_2, "T2", configMINIMAL_STACK_SIZE, NULL, 3, NULL);
+    
     #elif FILOSOFOS == 1
     config_user_app();
     xTaskCreate(f_0, "f0", configMINIMAL_STACK_SIZE, NULL, 3, NULL);
     // Para os demais filósofos
+
+    #elif EXAMPLE_2 == 1
+    config_user_app();
+    
+    xTaskCreate(task_1, "T1", configMINIMAL_STACK_SIZE, NULL, 3, NULL);
+    xTaskCreate(task_2, "T2", configMINIMAL_STACK_SIZE, NULL, 3, NULL);
+
+    #elif EXAMPLE_3 == 1
+    config_user_app();
+    
+    xTaskCreate(task_1, "T1", configMINIMAL_STACK_SIZE, NULL, 3, NULL);
+    xTaskCreate(task_2, "T2", configMINIMAL_STACK_SIZE, NULL, 3, NULL);
+
+    #elif EXAMPLE_4 == 1
+    config_user_app();
+    
+    xTaskCreate(task_1, "T1", configMINIMAL_STACK_SIZE, NULL, 3, NULL);
+    xTaskCreate(task_2, "T2", configMINIMAL_STACK_SIZE, NULL, 3, NULL);
+
+    #elif ALARME == 1
+    config_user_app();
+    
+    xTaskCreate(task_sensor, "T1", configMINIMAL_STACK_SIZE, NULL, 3, NULL);
+    xTaskCreate(task_lampada, "T2", configMINIMAL_STACK_SIZE, NULL, 3, NULL);
+    xTaskCreate(task_speaker, "T3", configMINIMAL_STACK_SIZE, NULL, 3, NULL);
     
     #endif    
     
