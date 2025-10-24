abc5317bb274c4e49eff96706c11045b900dfe54
Daleffe <augustodaleffe@gmail.com>
2025-10-24 01:07:50 -0300

    corrigindo o problema do userapp

--- Trabalho_1.X/user_app.h
+++ Trabalho_1.X/user_app.h
@@ -3,9 +3,13 @@
 
 #include "os_config.h"
 #include "types.h"
+#include "mutex.h"
 
 #define EXAMPLE_1 NO
-#define EXAMPLE_2 YES
+#define EXAMPLE_2 NO
+#define TRABALHO_1 YES
+
+Mutex mutex;
 
 #if EXAMPLE_1 == YES
 
@@ -19,6 +23,16 @@ TASK tarefa_3(void);
 void config_app(void);
 
 TASK tarefa_1(void);
+
+#elif TRABALHO_1 == YES
+void config_app(void);
+
+unsigned int readADC(unsigned char canal);
+void __interrupt() TMR0_ISR(void);
+uint8_t convert_to_duty_cycle(double motor_value);
+
+TASK motors_control(void);
+TASK sensors_reading(void);
 
 #endif
 
