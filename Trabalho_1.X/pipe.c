36099f927873385145d5c76b5a2072f28fab916b
Daleffe <augustodaleffe@gmail.com>
2025-10-24 00:42:24 -0300

    sem problema no pipe

--- Trabalho_1.X/pipe.c
+++ Trabalho_1.X/pipe.c
@@ -6,7 +6,7 @@ void create_pipe(pipe_t *p)
 {
     p->pipe_pos_read    = 0;
     p->pipe_pos_write   = 0;
-    // Inicializa os semáforos de controle do pipe
+    // Inicializa os sem?foros de controle do pipe
     sem_init(&p->pipe_sem_read, 0);
     sem_init(&p->pipe_sem_write, PIPE_MAX_SIZE);
 }
@@ -15,13 +15,13 @@ void read_pipe(pipe_t *p, char *buffer)
 {
     di();
     
-    // Testa o semáforo de leitura
+    // Testa o sem?foro de leitura
     sem_wait(&p->pipe_sem_read);
     
     *buffer = p->pipe_data[p->pipe_pos_read];
     p->pipe_pos_read = (p->pipe_pos_read + 1) % PIPE_MAX_SIZE;
     
-    // Libera o semáforo da escrita
+    // Libera o sem?foro da escrita
     sem_post(&p->pipe_sem_write);
     
     ei();
@@ -31,13 +31,13 @@ void write_pipe(pipe_t *p, char buffer)
 {
     di();
     
-    // Testa o semáforo de escrita
+    // Testa o sem?foro de escrita
     sem_wait(&p->pipe_sem_write);
     
     p->pipe_data[p->pipe_pos_write] = buffer;
     p->pipe_pos_write = (p->pipe_pos_write + 1) % PIPE_MAX_SIZE;
     
-    // Libera o semáforo da leitura
+    // Libera o sem?foro da leitura
     sem_post(&p->pipe_sem_read);
     
     ei();    
